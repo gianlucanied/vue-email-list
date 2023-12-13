1cf4ae76040dc1ab9e49ab451c3c462e79ec3c0b
gianlucanied <gianluca.nieddu96@gmail.com>
2023-12-13 16:06:17 +0100

    Add: v-for in HTML and mounted in JS

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            listEmail: []
+        }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    this.listEmail.push(response.data.response);
+                })
         }
     }
 }).mount('#app')
